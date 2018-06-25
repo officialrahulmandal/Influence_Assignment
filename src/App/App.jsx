5e177f818b3967e6536f93459eb84fb7ca6fd014
rahul <officialrahulmandal@gmail.com>
2018-06-25 08:06:59 +0530

    Routing for SPA created

---
+++ src/App/App.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { Router, Route, NavLink, HashRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import { history } from '../_helpers';
+import { alertActions } from '../_actions';
+import { PrivateRoute } from '../_components';
+import { HomePage } from '../HomePage';
+import { LoginPage } from '../LoginPage';
+import { RegisterPage } from '../RegisterPage';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        const { dispatch } = this.props;
+        history.listen((location, action) => {
+            // clear alert on location change
+            dispatch(alertActions.clear());
+        });
+    }
+
+    render() {
+        const { alert } = this.props;
+        return (
+            <div className="jumbotron">
+                <div className="container">
+                    <div className="col-sm-8 col-sm-offset-2">
+                        {alert.message &&
+                            <div className={`alert ${alert.type}`}>{alert.message}</div>
+                        }
+                        <Router history={history}>
+                            <div>
+                                <nav className="navbar navbar-default">
+                                    <div className="container-fluid">
+                                        <ul className="nav navbar-nav">
+                                            <li><NavLink exact to="/">Home</NavLink></li>
+                                            <li><NavLink to="/login">Login</NavLink></li>
+                                            <li><NavLink to="/register">Register</NavLink></li>
+                                        </ul>
+                                    </div>
+                                </nav>
+                                <PrivateRoute exact path="/" component={HomePage} />
+                                <Route path="/login" component={LoginPage} />
+                                <Route path="/register" component={RegisterPage} />
+                            </div>
+                        </Router>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+function mapStateToProps(state) {
+    const { alert } = state;
+    return {
+        alert
+    };
+}
+
+const connectedApp = connect(mapStateToProps)(App);
+export { connectedApp as App }; 
