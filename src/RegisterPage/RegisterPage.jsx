262d506c2892cf21d7eb22d00065c838dbb28eb5
rahul <officialrahulmandal@gmail.com>
2018-06-25 09:27:32 +0530

    tested and deployed v1

---
+++ src/RegisterPage/RegisterPage.jsx
@@ -0,0 +1,154 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import { userActions } from '../_actions';
+
+class RegisterPage extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            user: {
+                firstName: '',
+                lastName: '',
+                email: '',
+                phoneNo: '',
+                address: '',
+                city: '',
+                postalCode: '',
+                country: '',
+                comments: '',
+                username: '',
+                password: ''
+            },
+            submitted: false
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        const { name, value } = event.target;
+        const { user } = this.state;
+        this.setState({
+            user: {
+                ...user,
+                [name]: value
+            }
+        });
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+
+        this.setState({ submitted: true });
+        const { user } = this.state;
+        const { dispatch } = this.props;
+        if (user.firstName && user.lastName && user.username && user.password && user.email && user.phoneNo && user.address && user.city && user.postalCode && user.country && user.comments) {
+            dispatch(userActions.register(user));
+        }
+    }
+
+    render() {
+        const { registering  } = this.props;
+        const { user, submitted } = this.state;
+        return (
+            <div className="col-md-6 col-md-offset-3">
+                <h2>Register</h2>
+                <form name="form" onSubmit={this.handleSubmit}>
+                    <div className={'form-group' + (submitted && !user.firstName ? ' has-error' : '')}>
+                        <label htmlFor="firstName">First Name</label>
+                        <input type="text" className="form-control" name="firstName" value={user.firstName} onChange={this.handleChange} />
+                        {submitted && !user.firstName &&
+                            <div className="help-block">First Name is required</div>
+                        }
+                    </div>
+                    <div className={'form-group' + (submitted && !user.lastName ? ' has-error' : '')}>
+                        <label htmlFor="lastName">Last Name</label>
+                        <input type="text" className="form-control" name="lastName" value={user.lastName} onChange={this.handleChange} />
+                        {submitted && !user.lastName &&
+                            <div className="help-block">Last Name is required</div>
+                        }
+                    </div>
+                    <div className={'form-group' + (submitted && !user.email ? ' has-error' : '')}>
+                        <label htmlFor="email">E-Mail</label>
+                        <input type="email" className="form-control" name="email" value={user.email} onChange={this.handleChange} />
+                        {submitted && !user.email &&
+                            <div className="help-block">E-Mail is required</div>
+                        }
+                    </div>
+                    <div className={'form-group' + (submitted && !user.phoneNo.length === 10 ? ' has-error' : '')}>
+                        <label htmlFor="phoneNo">Phone No.</label>
+                        <input type="Number" className="form-control" name="phoneNo" value={user.phoneNo} onChange={this.handleChange} />   
+                    </div>
+                    <div className={'form-group' + (submitted && !user.address ? ' has-error' : '')}>
+                        <label htmlFor="address">Address</label>
+                        <input type="text" className="form-control" name="address" value={user.address} onChange={this.handleChange} />
+                        {submitted && !user.address &&
+                            <div className="help-block">Address is required</div>
+                        }
+                    </div>
+                    <div className={'form-group' + (submitted && !user.city ? ' has-error' : '')}>
+                        <label htmlFor="city">City</label>
+                        <input type="text" className="form-control" name="city" value={user.city} onChange={this.handleChange} />
+                        {submitted && !user.city &&
+                            <div className="help-block">City is required</div>
+                        }
+                    </div>
+                    <div className={'form-group' + (submitted && !user.postalCode ? ' has-error' : '')}>
+                        <label htmlFor="postalCode">Postal Code</label>
+                        <input type="Number" className="form-control" name="postalCode" value={user.postalCode} onChange={this.handleChange} />
+                        {submitted && !user.postalCode &&
+                            <div className="help-block">Postal Code is required</div>
+                        }
+                    </div>
+                    <div className={'form-group' + (submitted && !user.country ? ' has-error' : '')}>
+                        <label htmlFor="country">Country</label>
+                        <input type="text" className="form-control" name="country" value={user.country} onChange={this.handleChange} />
+                        {submitted && !user.country &&
+                            <div className="help-block">Country Name is required</div>
+                        }
+                    </div>
+                    <div className={'form-group'}>
+                        <label htmlFor="comments">Comments</label>
+                        <input type="text" className="form-control" name="comments" value={user.comments} onChange={this.handleChange} />
+                        
+                    </div>
+                    <div className={'form-group' + (submitted && !user.username ? ' has-error' : '')}>
+                        <label htmlFor="username">Username</label>
+                        <input type="text" className="form-control" name="username" value={user.username} onChange={this.handleChange} />
+                        {submitted && !user.username &&
+                            <div className="help-block">Username is required</div>
+                        }
+                    </div>
+                    <div className={'form-group' + (submitted && !user.password ? ' has-error' : '')}>
+                        <label htmlFor="password">Password</label>
+                        <input type="password" className="form-control" name="password" value={user.password} onChange={this.handleChange} />
+                        {submitted && !user.password &&
+                            <div className="help-block">Password is required</div>
+                        }
+                    </div>
+                    <div className="form-group">
+                        <button className="btn btn-primary">Register</button>
+                        {registering && 
+                            <img src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
+                        }
+                        <Link to="/login" className="btn btn-link">Cancel</Link>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+
+function mapStateToProps(state) {
+    const { registering } = state.registration;
+    return {
+        registering
+    };
+}
+
+const connectedRegisterPage = connect(mapStateToProps)(RegisterPage);
+export { connectedRegisterPage as RegisterPage };
